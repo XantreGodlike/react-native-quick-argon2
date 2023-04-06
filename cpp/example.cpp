d0c22984decdb404b2f65b79fdc821329950393f
XantreGodlike <valerik007.vs@gmail.com>
2023-04-07 01:24:02 +0300

    argon2 repo added && install added

--- cpp/example.cpp
+++ cpp/example.cpp
@@ -1,6 +1,8 @@
 #include "example.h"
+
 #include <jsi/jsi.h>
-#include<thread>
+
+#include <thread>
 using namespace facebook::jsi;
 using namespace std;
 
@@ -8,84 +10,64 @@ namespace example {
 
 void install(Runtime &jsiRuntime) {
 
-    auto helloWorld = Function::createFromHostFunction(jsiRuntime,
-                                                       PropNameID::forAscii(jsiRuntime,
-                                                                            "helloWorld"),
-                                                       0,
-                                                       [](Runtime &runtime,
-                                                          const Value &thisValue,
-                                                          const Value *arguments,
-                                                          size_t count) -> Value {
-        string helloworld = "helloworld";
+    // auto helloWorld = Function::createFromHostFunction(
+    //     jsiRuntime, PropNameID::forAscii(jsiRuntime, "helloWorld"), 0,
+    //     [](Runtime &runtime, const Value &thisValue, const Value *arguments,
+    //        size_t count) -> Value {
+    //         string helloworld = "helloworld";
 
+    //         return Value(runtime, String::createFromUtf8(runtime, helloworld));
+    //     });
 
-        return Value(runtime,
-                     String::createFromUtf8(
-                                            runtime,
-                                            helloworld));
+    // jsiRuntime.global().setProperty(jsiRuntime, "helloWorld", move(helloWorld));
 
-    });
+    // auto multiply = Function::createFromHostFunction(
+    //     jsiRuntime, PropNameID::forAscii(jsiRuntime, "multiply"), 2,
+    //     [](Runtime &runtime, const Value &thisValue, const Value *arguments,
+    //        size_t count) -> Value {
+    //         int x = arguments[0].getNumber();
+    //         int y = arguments[1].getNumber();
 
-    jsiRuntime.global().setProperty(jsiRuntime, "helloWorld", move(helloWorld));
+    //         return Value(x * y);
+    //     });
 
-    auto multiply = Function::createFromHostFunction(jsiRuntime,
-                                                     PropNameID::forAscii(jsiRuntime,
-                                                                          "multiply"),
-                                                     2,
-                                                     [](Runtime &runtime,
-                                                        const Value &thisValue,
-                                                        const Value *arguments,
-                                                        size_t count) -> Value {
-        int x = arguments[0].getNumber();
-        int y = arguments[1].getNumber();
+    // jsiRuntime.global().setProperty(jsiRuntime, "multiply", move(multiply));
 
-        return Value(x * y);
+    // auto multiplyWithCallback = Function::createFromHostFunction(
+    //     jsiRuntime, PropNameID::forAscii(jsiRuntime, "multiplyWithCallback"), 3,
+    //     [](Runtime &runtime, const Value &thisValue, const Value *arguments,
+    //        size_t count) -> Value {
+    //         int x = arguments[0].getNumber();
+    //         int y = arguments[1].getNumber();
 
-    });
+    //         arguments[2].getObject(runtime).getFunction(runtime).call(runtime,
+    //                                                                   x * y);
 
-    jsiRuntime.global().setProperty(jsiRuntime, "multiply", move(multiply));
+    //         return Value();
+    //     });
 
-    auto multiplyWithCallback = Function::createFromHostFunction(jsiRuntime,
-                                                                 PropNameID::forAscii(jsiRuntime,
-                                                                                      "multiplyWithCallback"),
-                                                                 3,
-                                                                 [](Runtime &runtime,
-                                                                    const Value &thisValue,
-                                                                    const Value *arguments,
-                                                                    size_t count) -> Value {
-        int x = arguments[0].getNumber();
-        int y = arguments[1].getNumber();
+    // jsiRuntime.global().setProperty(jsiRuntime, "multiplyWithCallback",
+    //                                 move(multiplyWithCallback));
 
-        arguments[2].getObject(runtime).getFunction(runtime).call(runtime, x * y);
+    // auto foo = Function::createFromHostFunction(
+    //     jsiRuntime, PropNameID::forAscii(jsiRuntime, "foo"), 1,
+    //     [](Runtime &runtime, const Value &thisValue, const Value *arguments,
+    //        size_t count) -> Value {
+    //         auto userCallbackRef =
+    //             std::make_shared<Object>(arguments[0].getObject(runtime));
 
-        return Value();
+    //         auto f = [&runtime](shared_ptr<Object> userCallbackRef) {
+    //             auto val = String::createFromUtf8(runtime, "hello world");
+    //             auto error = Value::undefined();
+    //             userCallbackRef->asFunction(runtime).call(runtime, error, val);
+    //         };
 
-    });
+    //         std::thread thread_object(f, userCallbackRef);
+    //         thread_object.detach();
 
-    jsiRuntime.global().setProperty(jsiRuntime, "multiplyWithCallback", move(multiplyWithCallback));
-
-
-    auto foo = Function::createFromHostFunction(
-            jsiRuntime,
-            PropNameID::forAscii(jsiRuntime, "foo"),
-            1,
-            [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-
-                auto userCallbackRef = std::make_shared<Object>(arguments[0].getObject(runtime));
-
-                auto f = [&runtime](shared_ptr<Object> userCallbackRef) {
-                    auto val = String::createFromUtf8(runtime, "hello world");
-                    auto error = Value::undefined();
-                    userCallbackRef->asFunction(runtime).call(runtime, error, val);
-                };
-
-                std::thread thread_object(f,userCallbackRef);
-                thread_object.detach();
-
-                return Value::undefined();
-            }
-    );
-    jsiRuntime.global().setProperty(jsiRuntime, "foo", std::move(foo));
+    //         return Value::undefined();
+    //     });
+    // jsiRuntime.global().setProperty(jsiRuntime, "foo", std::move(foo));
 }
 
-}
+}  // namespace example
